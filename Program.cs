0e99f90042beb4cca519415deeaa264405c2a9fc
beyzabzr <113631271+beyzabzr@users.noreply.github.com>
2023-11-15 16:52:31 +0300

    main

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace diziler_array_metodlari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayiDizisi = { 23, 12, 4, 86, 72, 3, 11, 17 };
+            Console.WriteLine("*** Sirasiz Dizi ***");
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            //Sort
+            Console.WriteLine("*** Sirali Dizi ***");
+            Array.Sort(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            //Clear
+            Console.WriteLine("*** Array Clear ***");
+            //sayiDizisi elemanlarini kullanarak 2.indexten itibaren 2. elemani 0'lar
+            Array.Clear(sayiDizisi, 2, 2);
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            //reverse
+            Console.WriteLine("*** Array Reverse ***");
+            Array.Reverse(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            //indexOf
+            Console.WriteLine("*** Array IndexOf ***");
+            Console.WriteLine(Array.IndexOf(sayiDizisi, 23));
+
+            //Resize
+            Console.WriteLine("*** Array Resize ***");
+            Array.Resize<int>(ref sayiDizisi, 9);
+            sayiDizisi[8] = 99;
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+        }
+    }
+}
